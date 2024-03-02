bbe23ea4c439aaaf47412a5eb855d165ed0aa095
salam <salamfoon@gmail.com>
2024-03-02 16:47:50 +0200

    first draft

---
+++ injector.py
@@ -0,0 +1,23 @@
+import sys
+
+def inject_binary(target, payload):
+    # Read the payload
+    with open(payload, 'rb') as payload_file:
+        payload = payload_file.read()
+        
+    # Read the target and append the payload
+    with open(target, 'ab') as target_file:
+        target_file.write(payload)
+        
+    print('Injected {payload} into {target}'.format(payload=payload, target=target))
+    
+    
+if __name__ == '__main__':
+    if len (sys.argv) != 3:
+        print('Usage: python injector.py <target> <payload>')
+        sys.exit(1)
+        
+    target = sys.argv[1]
+    payload = sys.argv[2]
+    
+    inject_binary(target, payload)
